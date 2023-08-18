2c025bbbdb7a6d932d23f584cceb860253be5fd8
Emma TREMLET <elecuelle001@ensc.fr>
2023-08-18 14:46:34 +0200

    add a page

---
+++ src/pages/PhotoDeProfil.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Header from '../components/Header.vue'
+
+</script>
+
+
+
+<template>
+    
+    <div class="body">
+    <RouterView>
+        <Header></Header>
+        <br>
+            <img src="../assets/Images/Photo_de_profil.JPG" alt="Photo d'une femme chatain qui sourit" >  
+        <br>
+        <RouterLink class="home" to="/">Retourner à la page d'acceuil</RouterLink>
+    </RouterView>
+    </div>
+   
+</template>
+
+
+<style scoped>
+.home{
+    margin-top:100px;
+    margin-left:27%;
+}
+.home:hover{
+    background-color:transparent;
+    color:#f0eff4;
+}
+
+img{
+    margin-top:5%;
+    margin-left:15%;
+}
+
+.body{
+    width: 100vw;
+    height:100vh;
+}
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../pages/Home.vue'
 import NotFound from '../pages/NotFound.vue'
+import PhotoDeProfil from '../pages/PhotoDeProfil.vue'
 
 
 const routes = [
   {
     path: "/",
-      name: "Accueil",
-      component: Home,
+    name: "Accueil",
+    component: Home,
+  },
+  {
+    path: "/PhotoDeProfil",
+    name: "PhotoDeProfil",
+    component: PhotoDeProfil,
   },
   {
     path: "/:catchAll(.*)",
